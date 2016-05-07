cfbf198dd2494b1ed338346d78e706fce26dd1e6
Andrej <vehar.andrej@gmail.com>
2016-05-07 08:24:17 +0000

    Zaključek dela na II. nalogi

--- eracun.js
+++ eracun.js
@@ -47,7 +47,11 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
-  pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
+  if(!zahteva.session.stranka) {
+    odgovor.redirect('/prijava');
+  }
+  else {
+    pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
           Artist.Name AS izvajalec, Track.UnitPrice * " +
           razmerje_usd_eur + " AS cena, \
           COUNT(InvoiceLine.InvoiceId) AS steviloProdaj, \
@@ -67,7 +71,8 @@ streznik.get('/', function(zahteva, odgovor) {
           vrstice[i].stopnja = davcnaStopnja(vrstice[i].izvajalec, vrstice[i].zanr);
         odgovor.render('seznam', {seznamPesmi: vrstice});
       }
-  })
+    })
+  }
 })
 
 // Dodajanje oz. brisanje pesmi iz košarice
@@ -233,12 +238,15 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
+    zahteva.session.stranka = polja.seznamStrank;
     odgovor.redirect('/')
   });
 })
 
 // Odjava stranke
 streznik.post('/odjava', function(zahteva, odgovor) {
+    zahteva.session.stranka = null;
+    zahteva.session.kosarica = null;
     odgovor.redirect('/prijava') 
 })
 
