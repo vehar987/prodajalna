68afa6f8042f3c3612ffd0fe11e489b417365a3f
Andrej <vehar.andrej@gmail.com>
2016-05-07 09:23:51 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,9 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if(napaka) callback(false)
+      else callback(vrstice)
+      //console.log(vrstice);
     })
 }
 
@@ -142,13 +144,25 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if(napaka) callback(false)
+      else callback(vrstice)
+      //console.log(vrstice);
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+  var form = new formidable.IncomingForm();
+  form.parse(zahteva, function (napaka1, polja, datoteke) {
+    var racunID = polja.seznamRacunov;
+    pesmiIzRacuna(racunID, function(pesmi) {
+      strankaIzRacuna(racunID, function(stranka) {
+        odgovor.setHeader('content-type', 'text/xml');
+        odgovor.render('eslog', {vizualiziraj: true, postavkeRacuna: pesmi, customerData: stranka[0]});
+      });
+    });
+  });
+  //odgovor.end();
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
